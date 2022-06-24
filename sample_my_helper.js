16eb8f786d700d3a3d9d345ee706717ce6eb036f
patricia_xiao <patriciaxiao@g.ucla.edu>
2022-06-23 23:11:57 -0400

    updates

---
+++ sample_my_helper.js
@@ -0,0 +1,2 @@
+var ImageList = ["./sample_img/0.jpeg", "./sample_img/1.jpeg", "./sample_img/2.jpeg", "./sample_img/3.jpeg"];
+var LabelList = ["Here's the overlay text 0", "Here's the overlay text 1", "Here's the overlay text 2", "Here's the overlay text 3"];
